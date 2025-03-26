39061050ea095b94d479e406e1e2fb38c3ac163c
Bruno Benavent Colomo <brunobenaventcolomo@iMac-de-Bruno.local>
2025-03-26 13:29:02 +0100

    Conexión a la db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,9 @@
+import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize'
+dotenv.config()
 
+const db = new Sequelize(process.env.DATABASE_URL)
 
-
-const db = new Sequelize('postgresql://rest_api_node_typescript_ua6i_user:bZD06THevBJjS8bOgGLITRXm1YAs1Xh9@dpg-cvhk5l9c1ekc738ce36g-a.frankfurt-postgres.render.com/rest_api_node_typescript_ua6i', {
-    dialectOptions:{
-        ssl: {
-            require: false
-        }
-    }
-})
 
 
 export default db
